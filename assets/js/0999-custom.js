310fa8c92d0576f342c4c98589c352d697d47df9
deepfry <jj@4cc.co>
2017-09-23 10:22:48 -0700

    added share buttons

--- assets/js/0999-custom.js
+++ assets/js/0999-custom.js
@@ -6,3 +6,9 @@ if (validateFields($("#contactFormCustom")) !== 1) return
 $('input, textarea').keyup(function(){
 	$(this).removeClass('error')
 })
+
+$(document).ready(function(){
+	$('#share-icons .facebook-share').attr('href', "https://www.facebook.com/dialog/share?app_id=158575354726891&display=popup&href="+window.location.href+"&quote="+encodeURIComponent($('#page-description').attr('content')))
+	$('#share-icons .twitter-share').attr('href', "http://twitter.com/share?url="+window.location.href+"&hashtags=rushellekucala,thedragonofdoom")
+
+})
